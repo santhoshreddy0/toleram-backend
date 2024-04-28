38d5d146281cf595e287e00576377e09fc3e3051
Santhosh Reddy <mail2myweb@gmail.com>
2024-04-28 21:17:24 +0530

    match questions + bets api added

--- controllers/matches.js
+++ controllers/matches.js
@@ -1,19 +1,197 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const pool = require('../db');
+const pool = require("../db");
 const { verifyToken } = require("../middleware/middleware");
 
 router.get("/", async (req, res) => {
-  try {
-    // Retrieve all available matches from the matches table
-    const query = "SELECT * FROM matches";
-    const [rows] = await pool.execute(query);
+    try {
+        // Retrieve all available matches from the matches table
+        const query = `SELECT matches.*, 
+              teams.team_name AS team_one_name,
+              teams.team_logo AS team_one_logo,
+              team2.team_name AS team_two_name,
+              team2.team_logo AS team_two_logo
+              FROM matches 
+              JOIN teams 
+                ON matches.team_one = teams.id 
+              JOIN teams AS team2 
+                ON matches.team_two = team2.id
+            `;
+        const [rows] = await pool.execute(query);
 
-    res.json({ matches: rows });
-  } catch (error) {
-    console.error("Error executing query", error);
-    res.status(500).json({ message: "Internal server error" });
-  }
+        res.json({ matches: rows });
+    } catch (error) {
+        console.error("Error executing query", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+});
+
+router.get("/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        // Retrieve all available matches from the matches table
+        const query = `SELECT matches.*, 
+              teams.team_name AS team_one_name,
+              teams.team_logo AS team_one_logo,
+              team2.team_name AS team_two_name,
+              team2.team_logo AS team_two_logo
+              FROM matches 
+              JOIN teams 
+                ON matches.team_one = teams.id 
+              JOIN teams AS team2 
+                ON matches.team_two = team2.id
+              WHERE matches.id = ?
+            `;
+        const [rows] = await pool.execute(query, [id]);
+
+        if (rows.length === 0) {
+            return res.status(404).json({ message: "Match not found" });
+        }
+
+        res.json({ match: rows[0] });
+    } catch (error) {
+        console.error("Error executing query", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+});
+
+router.get("/:id/questions", async (req, res) => {
+    try {
+        const { id: match_id } = req.params;
+        // Retrieve all available matches from the matches table
+        const query = `SELECT * FROM match_questions JOIN question_options ON match_questions.id = question_options.question_id  WHERE match_id = ? `;
+        const [rows] = await pool.execute(query, [match_id]);
+
+        const map = {};
+        rows.forEach((row) => {
+            if (!map[row.question_id]) {
+                map[row.question_id] = {
+                    question: row.question,
+                    options: [],
+                };
+            }
+            map[row.question_id].options.push({
+                id: row.id,
+                option: row.options,
+                odds: row.odds,
+            });
+        });
+
+        res.json({ questions: map });
+    } catch (error) {
+        console.error("Error executing query", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+});
+
+router.get("/:id/bet", verifyToken, async (req, res) => {
+    try {
+        const { id: match_id } = req.params;
+        const user_id = req.user.id;
+        const query = "Select * from matches where id = ?";
+        const [rows] = await pool.execute(query, [match_id]);
+        if (rows.length === 0) {
+            return res.status(404).json({ message: "Match not found" });
+        }
+
+        const match = rows[0];
+
+
+        const quesstionQuery =
+            "Select id from match_questions where match_id = ?";
+        const [questionRows] = await pool.execute(quesstionQuery, [match_id]);
+
+        if (questionRows.length === 0) {
+            return res
+                .status(404)
+                .json({ message: "No questions found for this match" });
+        }
+
+        const rowCheckingQuery =
+            "SELECT * FROM match_bets WHERE user_id = ? AND match_id = ?";
+        const rowValues = [user_id, match_id];
+        const [existing_bets] = await pool.execute(rowCheckingQuery, rowValues);
+
+        if (existing_bets.length === 0) {
+            return res.status(200).json({ bets: {} });
+        } else {
+            return res.status(200).json({ bets: JSON.parse(existing_bets[0].answers) });
+        }
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+});
+router.put("/:id/bet", verifyToken, async (req, res) => {
+    try {
+        const { id: match_id } = req.params;
+        const user_id = req.user.id;
+        const { bets } = req.body;
+        if (!bets || bets?.length === 0) {
+            return res.status(400).json({ message: "No bets provided" });
+        }
+
+        const query = "Select * from matches where id = ?";
+        const [rows] = await pool.execute(query, [match_id]);
+        if (rows.length === 0) {
+            return res.status(404).json({ message: "Match not found" });
+        }
+
+        const match = rows[0];
+        if (match.can_bet === 0) {
+            return res
+                .status(400)
+                .json({ message: "Betting is closed for this match" });
+        }
+
+        const quesstionQuery =
+            "Select id from match_questions where match_id = ?";
+        const [questionRows] = await pool.execute(quesstionQuery, [match_id]);
+
+        if (questionRows.length === 0) {
+            return res
+                .status(404)
+                .json({ message: "No questions found for this match" });
+        }
+
+        const questionMap = {};
+        questionRows.forEach((row) => {
+            let data = {};
+            data["option"] = bets[row.id]?.option || null;
+            data["amount"] = bets[row.id]?.amount || 0;
+            questionMap[row.id] = data;
+        });
+
+        const rowCheckingQuery =
+            "SELECT * FROM match_bets WHERE user_id = ? AND match_id = ?";
+        const rowValues = [user_id, match_id];
+        const [existing_bets] = await pool.execute(rowCheckingQuery, rowValues);
+
+        if (existing_bets.length === 0) {
+            const postQuery =
+                "INSERT INTO match_bets (user_id, match_id,answers) VALUES (?, ? , ?) ";
+            await pool.query(postQuery, [
+                user_id,
+                match_id,
+                JSON.stringify(questionMap),
+            ]);
+            return res.status(200).json({ message: "Bet placed successfully" });
+        } else {
+            const putQuery =
+                "UPDATE match_bets SET answers = ? WHERE user_id = ? AND match_id = ?";
+            await pool.query(putQuery, [
+                JSON.stringify(questionMap),
+                user_id,
+                match_id,
+            ]);
+            return res
+                .status(200)
+                .json({ message: "Bet updated successfully" });
+        }
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: "Internal server error" });
+    }
 });
 
 module.exports = router;
--- newsql.sql
+++ newsql.sql
@@ -1,24 +1,86 @@
 CREATE DATABASE tolaram;
+
 USE tolaram;
 
-CREATE TABLE users (
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE
+  users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) DEFAULT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+  );
+
+INSERT INTO
+  `users` (`name`, `email`, `password`)
+VALUES
+  ('santhosh', 'admin@gmail.com', '123456789');
+
+DROP TABLE IF EXISTS teams;
+
+CREATE TABLE
+  teams (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    team_name VARCHAR(255) NOT NULL,
+    team_logo VARCHAR(255) NOT NULL
+  );
+
+INSERT INTO
+  `teams` (`team_name`, `team_logo`)
+ VALUES
+ ('Addmie & Indomie Warriors', 'indomie_&_ADDMIE.png'),
+ ('GPL & Corporate Falcons', 'corporate.png'),
+ ('Colgate White Warriors', 'colgate.png'),
+ ('ROWA & Minimie Vikings', 'ROWA_&_minimie.png'),
+ ('Lucky Stars', 'lucky_stars.png'),
+ ('Power Brancos', 'power_brancos.png');
+ 
+
+ 
+DROP TABLE IF EXISTS matches;
+
+CREATE TABLE
+  matches (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    team_one INT REFERENCES teams (id),
+    team_two INT REFERENCES teams (id),
+    match_title VARCHAR(250) NOT NULL,
+    match_time DATETIME NOT NULL,
+    can_bet enum ('0', '1') DEFAULT '0',
+    can_show enum ('0', '1') DEFAULT '0'
+  );
+
+INSERT INTO `matches`(team_one, team_two, match_title, match_time, can_bet, can_show) VALUES
+(1, 2, 'Addmie & Indomie Warriors vs GPL & Corporate Falcons', '2021-09-10 12:00:00', '1', '1'),
+(3, 4, 'Colgate White Warriors vs ROWA & Minimie Vikings', '2021-09-10 12:00:00', '1', '1'),
+(5, 6, 'Lucky Stars vs Power Brancos', '2021-09-10 12:00:00', '1', '1');
+
+
+DROP TABLE IF EXISTS match_questions;
+
+CREATE TABLE match_questions (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(100) DEFAULT NULL,
-  email VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL
+  match_id INT REFERENCES matches (id),
+  question VARCHAR(255) NOT NULL,
+  can_show enum ('0', '1') DEFAULT '0',
+  correct_option VARCHAR(255) NOT NULL
 );
 
-INSERT INTO `users`(`name`, `email`, `password`) VALUES 
-('santhosh', 'admin@gmail.com', '123456789' );
+DROP TABLE IF EXISTS question_options;
 
-DROP TABLE matches;
-CREATE TABLE matches (
+CREATE TABLE question_options (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  team_one VARCHAR(255) NOT NULL,
-  team_two VARCHAR(255) NOT NULL,
-  match_start_time DAT NOT NULL,
-  match_title VARCHAR(50) NOT NULL,
-  status enum('0','1') DEFAULT '0',
-  questions JSON NOT NULL
+  question_id INT REFERENCES match_questions (id),
+  options VARCHAR(255) NOT NULL,
+  odds DOUBLE NOT NULL
 );
 
+DROP TABLE IF EXISTS match_bets;
+
+CREATE TABLE match_bets (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  user_id INT REFERENCES users (id),
+  match_id INT REFERENCES matches (id),
+  answers JSON NOT NULL
+);
