d64e580e47837ad72d8436fb4e625162a87b5848
mintureddy25 <chappetasaitejareddy@gmail.com>
2025-03-18 21:51:04 +0530

    admin apis added

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    database: {
+      host: 'saitejareddy-chappetasaitejareddy-75bf.i.aivencloud.com',
+      name: 'tolaram',
+      user: 'avnadmin',
+      password: 'AVNS_D_8TtIDpt1WUkZ9a3Qy',
+      port: 25794
+    },
+    server_port : 3000,
+    jwtSecret: 'secret'
+  };
+  
