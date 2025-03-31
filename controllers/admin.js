c85d6a7d8bc911d95c826b6f9f323aa74830c9be
mintureddy25 <chappetasaitejareddy@gmail.com>
2025-04-01 01:40:48 +0530

    added match update

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,8 +1,6 @@
-
 const express = require("express");
 const router = express.Router();
 const pool = require("../db");
-
 
 function validateName(name) {
   if (!name || name.length < 3) return false;
@@ -13,7 +11,6 @@ function validateString(string) {
   if (!string || string.trim().length === 0) return false;
   return true;
 }
-
 
 router.post("/matches", async (req, res) => {
   const {
@@ -42,14 +39,16 @@ router.post("/matches", async (req, res) => {
   if (teamOneId == teamTwoId) {
     validationErrors.push("Please select different teams");
   }
-  if(betStatus && !['dont_process', 'process', 'completed'].includes(betStatus)){
+  if (
+    betStatus &&
+    !["dont_process", "process", "completed"].includes(betStatus)
+  ) {
     validationErrors.push("Invalid bet status");
   }
 
   if (validationErrors.length > 0) {
     return res.status(400).json({ errors: validationErrors });
   }
-  
 
   // Validate match time format (assume it's in ISO string format)
   const matchDate = new Date(matchTime);
@@ -58,7 +57,10 @@ router.post("/matches", async (req, res) => {
   }
 
   // Set the match_time to UTC format (use toISOString() to get UTC)
-  const matchTimeUtc = new Date(matchDate.toISOString()).toISOString().slice(0, 19).replace('T', ' ');
+  const matchTimeUtc = new Date(matchDate.toISOString())
+    .toISOString()
+    .slice(0, 19)
+    .replace("T", " ");
 
   try {
     const [teamRows] = await pool.execute(
@@ -71,7 +73,9 @@ router.post("/matches", async (req, res) => {
     }
 
     if (teamOneId == teamTwoId) {
-      return res.status(400).json({ message: "Please select two different teams" });
+      return res
+        .status(400)
+        .json({ message: "Please select two different teams" });
     }
 
     const [insertResult] = await pool.execute(
@@ -116,7 +120,10 @@ router.patch("/matches/:id", async (req, res) => {
     validationErrors.push("Please select different teams.");
   }
 
-  if (betStatus && !['dont_process', 'process', 'completed'].includes(betStatus)) {
+  if (
+    betStatus &&
+    !["dont_process", "process", "completed"].includes(betStatus)
+  ) {
     validationErrors.push("Invalid bet status.");
   }
 
@@ -133,27 +140,46 @@ router.patch("/matches/:id", async (req, res) => {
   }
 
   try {
-   
-    const [matchRows] = await pool.execute("SELECT * FROM matches WHERE id = ?", [matchId]);
+    const [matchRows] = await pool.execute(
+      "SELECT * FROM matches WHERE id = ?",
+      [matchId]
+    );
 
     if (matchRows.length === 0) {
       return res.status(404).json({ message: "Match not found." });
     }
 
-    
-    if (teamOneId || teamTwoId) {
-      const [teamRows] = await pool.execute("SELECT * FROM teams WHERE id IN (?, ?)", [teamOneId, teamTwoId]);
+    const { team_one: existingTeamOne, team_two: existingTeamTwo } =
+      matchRows[0];
 
-      if (teamRows.length !== 2) {
-        return res.status(400).json({ message: "One or both teams not found." });
-      }
+    const newTeamOneId = teamOneId || existingTeamOne;
+    const newTeamTwoId = teamTwoId || existingTeamTwo;
 
-      if (teamOneId === teamTwoId) {
-        return res.status(400).json({ message: "Please select two different teams." });
+    if (newTeamOneId === newTeamTwoId) {
+      return res
+        .status(400)
+        .json({ message: "Please select two different teams." });
+    }
+
+    const validTeams = [];
+    if (teamOneId) validTeams.push(teamOneId);
+    if (teamTwoId) validTeams.push(teamTwoId);
+
+    if (validTeams.length > 0) {
+      const [teamRows] = await pool.execute(
+        `SELECT id FROM teams WHERE id IN (${validTeams
+          .map(() => "?")
+          .join(", ")})`,
+        validTeams
+      );
+
+      if (teamRows.length !== validTeams.length) {
+        return res
+          .status(400)
+          .json({ message: "One or more selected teams not found." });
       }
     }
 
-    
     const updates = [];
     const updateValues = [];
 
@@ -171,7 +197,10 @@ router.patch("/matches/:id", async (req, res) => {
     }
     if (matchTime) {
       const matchDate = new Date(matchTime);
-      const matchTimeUtc = new Date(matchDate.toISOString()).toISOString().slice(0, 19).replace('T', ' ');
+      const matchTimeUtc = new Date(matchDate.toISOString())
+        .toISOString()
+        .slice(0, 19)
+        .replace("T", " ");
       updates.push("match_time = ?");
       updateValues.push(matchTimeUtc);
     }
@@ -188,12 +217,10 @@ router.patch("/matches/:id", async (req, res) => {
       updateValues.push(betStatus);
     }
 
-    
-    updates.push("WHERE id = ?");
     updateValues.push(matchId);
 
     // Update the match record
-    const query = `UPDATE matches SET ${updates.join(", ")} `;
+    const query = `UPDATE matches SET ${updates.join(", ")}  WHERE id = ? `;
     await pool.execute(query, updateValues);
 
     res.json({
@@ -206,8 +233,7 @@ router.patch("/matches/:id", async (req, res) => {
   }
 });
 
-
-router.post("/matches/:matchId/addQuestion",  async (req, res) => {
+router.post("/matches/:matchId/addQuestion", async (req, res) => {
   const { question, canShow, options } = req.body;
   const { matchId } = req.params;
 
@@ -240,10 +266,14 @@ router.post("/matches/:matchId/addQuestion",  async (req, res) => {
 
     options.forEach((option, index) => {
       if (!option.id || !option.option || !option.odds) {
-        validationErrors.push(`Option at index ${index} is missing id, option, or odds.`);
+        validationErrors.push(
+          `Option at index ${index} is missing id, option, or odds.`
+        );
       }
       if (idsSet.has(option.id)) {
-        validationErrors.push(`Option at index ${index} has a duplicate id: ${option.id}.`);
+        validationErrors.push(
+          `Option at index ${index} has a duplicate id: ${option.id}.`
+        );
       } else {
         idsSet.add(option.id);
       }
@@ -392,242 +422,246 @@ router.patch("/matches/:matchId/players/:playerId", async (req, res) => {
 });
 
 router.patch("/players/:playerId", async (req, res) => {
-    const { playerId } = req.params;
-    const { name, imageUrl, role  } = req.body;
-  
-    if (name && !validateName(name)) {
-      return res.status(400).json({ message: "Invalid player name" });
-    }
-  
-    if (imageUrl && !validateString(imageUrl)) {
-      return res.status(400).json({ message: "Invalid url" });
-    }
-    if (role && !['all-rounder', 'batsman', 'bowler', 'wicket-keeper'].includes(role)) {
-        return res.status(400).json({ message: "Invalid role for player" });
-      }
+  const { playerId } = req.params;
+  const { name, imageUrl, role } = req.body;
 
-  
-    try {
-      // Check if the team exists
-      const [playerRows] = await pool.execute(
-        "SELECT * FROM players WHERE id = ?",
-        [playerId]
-      );
-  
-      if (playerRows.length === 0) {
-        return res.status(404).json({ message: "Player not found" });
-      }
-  
-      // Update fields conditionally
-      const updateFields = [];
-      const updateValues = [];
-  
-      if (name) {
-        updateFields.push("name = ?");
-        updateValues.push(name);
-      }
-  
-      if (imageUrl) {
-        updateFields.push("player_logo = ?");
-        updateValues.push(imageUrl);
-      }
-  
-      if (role !== undefined) {
-        updateFields.push("player_role = ?");
-        updateValues.push(role);
-      }
-  
-      if (updateFields.length === 0) {
-        return res.status(400).json({ message: "No fields to update" });
-      }
-  
-      updateValues.push(playerId);
-  
-      const [updateResult] = await pool.execute(
-        `UPDATE players SET ${updateFields.join(", ")} WHERE id = ?`,
-        updateValues
-      );
-  
-      if (updateResult.affectedRows === 0) {
-        return res.status(400).json({ message: "Update failed" });
-      }
-  
-      res.json({
-        message: "Player updated successfully"
-      });
-    } catch (error) {
-      console.error("Error executing query", error);
-      res.status(500).json({ message: "Internal server error" });
+  if (name && !validateName(name)) {
+    return res.status(400).json({ message: "Invalid player name" });
+  }
+
+  if (imageUrl && !validateString(imageUrl)) {
+    return res.status(400).json({ message: "Invalid url" });
+  }
+  if (
+    role &&
+    !["all-rounder", "batsman", "bowler", "wicket-keeper"].includes(role)
+  ) {
+    return res.status(400).json({ message: "Invalid role for player" });
+  }
+
+  try {
+    // Check if the team exists
+    const [playerRows] = await pool.execute(
+      "SELECT * FROM players WHERE id = ?",
+      [playerId]
+    );
+
+    if (playerRows.length === 0) {
+      return res.status(404).json({ message: "Player not found" });
     }
-  });
-  
-  router.post("/teams", async (req, res) => {
-    const { name, imageUrl } = req.body;
-  
-    if (!validateName(name)) {
-      res.status(400).json({ message: "Invalid team name" });
-      return;
+
+    // Update fields conditionally
+    const updateFields = [];
+    const updateValues = [];
+
+    if (name) {
+      updateFields.push("name = ?");
+      updateValues.push(name);
     }
-  
-    if (!validateString(imageUrl)) {
-      res.status(400).json({ message: "Invalid url" });
-      return;
+
+    if (imageUrl) {
+      updateFields.push("player_logo = ?");
+      updateValues.push(imageUrl);
     }
-  
-    try {
-      const [teamRows] = await pool.execute(
-        "SELECT * FROM teams WHERE team_name = ?",
-        [name]
-      );
-  
-      if (teamRows.length > 0) {
-        res.status(400).json({ message: "Team already exists" });
-        return;
-      }
-  
-      const [insertResult] = await pool.execute(
-        "INSERT INTO teams (team_name, team_logo) VALUES (?, ?)",
-        [name, imageUrl]
-      );
-  
-      res.json({
-        message: "Team created successfully",
-        teamId: insertResult.insertId,
-      });
-    } catch (error) {
-      console.error("Error executing query", error);
-      res.status(500).json({ message: "Internal server error" });
+
+    if (role !== undefined) {
+      updateFields.push("player_role = ?");
+      updateValues.push(role);
     }
-  });
-  
-  router.patch("/teams/:teamId", async (req, res) => {
-      const { teamId } = req.params;
-      const { name, imageUrl, status } = req.body;
-    
-      if (name && !validateName(name)) {
-        return res.status(400).json({ message: "Invalid team name" });
-      }
-    
-      if (imageUrl && !validateString(imageUrl)) {
-        return res.status(400).json({ message: "Invalid url" });
-      }
-    
-      if (status && !['0', '1'].includes(status)) {
-        return res.status(400).json({ message: "Invalid operation" });
-      }
-    
-      try {
-        // Check if the team exists
-        const [teamRows] = await pool.execute(
-          "SELECT * FROM teams WHERE id = ?",
-          [teamId]
-        );
-    
-        if (teamRows.length === 0) {
-          return res.status(404).json({ message: "Team not found" });
-        }
-    
-        // Update fields conditionally
-        const updateFields = [];
-        const updateValues = [];
-    
-        if (name) {
-          updateFields.push("team_name = ?");
-          updateValues.push(name);
-        }
-    
-        if (imageUrl) {
-          updateFields.push("team_logo = ?");
-          updateValues.push(imageUrl);
-        }
-    
-        if (status !== undefined) {
-          updateFields.push("status = ?");
-          updateValues.push(status);
-        }
-    
-        if (updateFields.length === 0) {
-          return res.status(400).json({ message: "No fields to update" });
-        }
-    
-        // Add teamId to the end for where clause
-        updateValues.push(teamId);
-    
-        // Perform the update query
-        const [updateResult] = await pool.execute(
-          `UPDATE teams SET ${updateFields.join(", ")} WHERE id = ?`,
-          updateValues
-        );
-    
-        if (updateResult.affectedRows === 0) {
-          return res.status(400).json({ message: "Update failed" });
-        }
-    
-        res.json({
-          message: "Team updated successfully"
-        });
-      } catch (error) {
-        console.error("Error executing query", error);
-        res.status(500).json({ message: "Internal server error" });
-      }
+
+    if (updateFields.length === 0) {
+      return res.status(400).json({ message: "No fields to update" });
+    }
+
+    updateValues.push(playerId);
+
+    const [updateResult] = await pool.execute(
+      `UPDATE players SET ${updateFields.join(", ")} WHERE id = ?`,
+      updateValues
+    );
+
+    if (updateResult.affectedRows === 0) {
+      return res.status(400).json({ message: "Update failed" });
+    }
+
+    res.json({
+      message: "Player updated successfully",
     });
+  } catch (error) {
+    console.error("Error executing query", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
 
-    //createplayer
-      router.post("/teams/:teamId", async (req, res) => {
-        const { teamId } = req.params;
-        const { name, imageUrl , role} = req.body;
-      
-        if (!validateName(name)) {
-          return res.status(400).json({ message: "Invalid player name" });
-        }
-      
-        if (!validateString(imageUrl)) {
-          return res.status(400).json({ message: "Invalid url" });
-        }
-    
-        if (!role || !['all-rounder', 'batsman', 'bowler', 'wicket-keeper'].includes(role)) {
-            return res.status(400).json({ message: "Invalid role for player" });
-          }
-    
-      
-        try {
-          // Check if the team exists
-          const [teamRows] = await pool.execute(
-            "SELECT * FROM teams WHERE id = ?",
-            [teamId]
-          );
-      
-          if (teamRows.length === 0) {
-            return res.status(404).json({ message: "Team not found" });
-          }
-    
-          const [playerRows] = await pool.execute(
-            "SELECT * FROM players WHERE name = ? AND team_id = ?", 
-            [name, teamId]
-          );
-      
-          if (playerRows.length > 0) {
-            return res.status(400).json({ message: "Player with the name already exists in the team" });
-          }
-      
-          const [insertResult] = await pool.execute(
-            "INSERT INTO players (name, player_logo, team_id, player_role) VALUES (?, ?, ?, ?)",
-            [name, imageUrl, teamId, role]
-          );
-      
-          res.json({
-            message: "Player added successfully",
-            player: {
-              id: insertResult.insertId,
-              name,
-              imageUrl,
-              teamId,
-            },
-          });
-        } catch (error) {
-          console.error("Error executing query", error);
-          res.status(500).json({ message: "Internal server error" });
-        }
-      });
+router.post("/teams", async (req, res) => {
+  const { name, imageUrl } = req.body;
+
+  if (!validateName(name)) {
+    res.status(400).json({ message: "Invalid team name" });
+    return;
+  }
+
+  if (!validateString(imageUrl)) {
+    res.status(400).json({ message: "Invalid url" });
+    return;
+  }
+
+  try {
+    const [teamRows] = await pool.execute(
+      "SELECT * FROM teams WHERE team_name = ?",
+      [name]
+    );
+
+    if (teamRows.length > 0) {
+      res.status(400).json({ message: "Team already exists" });
+      return;
+    }
+
+    const [insertResult] = await pool.execute(
+      "INSERT INTO teams (team_name, team_logo) VALUES (?, ?)",
+      [name, imageUrl]
+    );
+
+    res.json({
+      message: "Team created successfully",
+      teamId: insertResult.insertId,
+    });
+  } catch (error) {
+    console.error("Error executing query", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+router.patch("/teams/:teamId", async (req, res) => {
+  const { teamId } = req.params;
+  const { name, imageUrl, status } = req.body;
+
+  if (name && !validateName(name)) {
+    return res.status(400).json({ message: "Invalid team name" });
+  }
+
+  if (imageUrl && !validateString(imageUrl)) {
+    return res.status(400).json({ message: "Invalid url" });
+  }
+
+  if (status && !["0", "1"].includes(status)) {
+    return res.status(400).json({ message: "Invalid operation" });
+  }
+
+  try {
+    // Check if the team exists
+    const [teamRows] = await pool.execute("SELECT * FROM teams WHERE id = ?", [
+      teamId,
+    ]);
+
+    if (teamRows.length === 0) {
+      return res.status(404).json({ message: "Team not found" });
+    }
+
+    // Update fields conditionally
+    const updateFields = [];
+    const updateValues = [];
+
+    if (name) {
+      updateFields.push("team_name = ?");
+      updateValues.push(name);
+    }
+
+    if (imageUrl) {
+      updateFields.push("team_logo = ?");
+      updateValues.push(imageUrl);
+    }
+
+    if (status !== undefined) {
+      updateFields.push("status = ?");
+      updateValues.push(status);
+    }
+
+    if (updateFields.length === 0) {
+      return res.status(400).json({ message: "No fields to update" });
+    }
+
+    // Add teamId to the end for where clause
+    updateValues.push(teamId);
+
+    // Perform the update query
+    const [updateResult] = await pool.execute(
+      `UPDATE teams SET ${updateFields.join(", ")} WHERE id = ?`,
+      updateValues
+    );
+
+    if (updateResult.affectedRows === 0) {
+      return res.status(400).json({ message: "Update failed" });
+    }
+
+    res.json({
+      message: "Team updated successfully",
+    });
+  } catch (error) {
+    console.error("Error executing query", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+//createplayer
+router.post("/teams/:teamId", async (req, res) => {
+  const { teamId } = req.params;
+  const { name, imageUrl, role } = req.body;
+
+  if (!validateName(name)) {
+    return res.status(400).json({ message: "Invalid player name" });
+  }
+
+  if (!validateString(imageUrl)) {
+    return res.status(400).json({ message: "Invalid url" });
+  }
+
+  if (
+    !role ||
+    !["all-rounder", "batsman", "bowler", "wicket-keeper"].includes(role)
+  ) {
+    return res.status(400).json({ message: "Invalid role for player" });
+  }
+
+  try {
+    // Check if the team exists
+    const [teamRows] = await pool.execute("SELECT * FROM teams WHERE id = ?", [
+      teamId,
+    ]);
+
+    if (teamRows.length === 0) {
+      return res.status(404).json({ message: "Team not found" });
+    }
+
+    const [playerRows] = await pool.execute(
+      "SELECT * FROM players WHERE name = ? AND team_id = ?",
+      [name, teamId]
+    );
+
+    if (playerRows.length > 0) {
+      return res
+        .status(400)
+        .json({ message: "Player with the name already exists in the team" });
+    }
+
+    const [insertResult] = await pool.execute(
+      "INSERT INTO players (name, player_logo, team_id, player_role) VALUES (?, ?, ?, ?)",
+      [name, imageUrl, teamId, role]
+    );
+
+    res.json({
+      message: "Player added successfully",
+      player: {
+        id: insertResult.insertId,
+        name,
+        imageUrl,
+        teamId,
+      },
+    });
+  } catch (error) {
+    console.error("Error executing query", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
 
 module.exports = router;
