95a63ba6af428ced26ed47f80b4bf7232d6a7c9a
mintureddy25 <chappetasaitejareddy@gmail.com>
2025-05-03 12:38:28 +0530

    bug fix

--- controllers/admin/analytics.js
+++ controllers/admin/analytics.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const pool = require("../../db");
 const { jsonParse } = require("../../utils");
+const RedisClient = require("../../redis");
 
 router.get("/matches/:matchId/bets", async (req, res) => {
   try {
